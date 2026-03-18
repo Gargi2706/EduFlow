8dfd446604e360a5300b360afc590bd73d1b8823
Naman Patel <namanpatel@Namans-MacBook-Air.local>
2026-03-18 12:16:36 +0530

    initialize navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Auth from "./pages/Auth";
+// import Auth from "./pages/Auth";
+// import Home from "./pages/Home";
+// import Courses from "./pages/Courses";
+// import CourseDetails from "./pages/CourseDetails";
 
-import Home from "./pages/Home";
-import Courses from "./pages/Courses";
-import CourseDetails from "./pages/CourseDetails";
-
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
 
-        
-        <Route path="/" element={<Auth />} /> 
-        <Route
+
+        {/* <Route path="/" element={<Auth />} />  */}
+        {/* <Route
           path="/home"
           element={
             <>
@@ -43,7 +43,7 @@ function App() {
               <CourseDetails />
             </>
           }
-        />
+        /> */}
 
       </Routes>
     </BrowserRouter>
--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -2,18 +2,13 @@ import { useState } from "react";
 import "../styles/auth.css";
 
 function Auth() {
-
   const [activeTab, setActiveTab] = useState("login");
 
   return (
     <div className="auth-container">
-
       <div className="auth-card">
-
         <div className="auth-left">
-
           <div className="tabs">
-
             <button
               className={activeTab === "login" ? "active" : ""}
               onClick={() => setActiveTab("login")}
@@ -27,37 +22,54 @@ function Auth() {
             >
               Register
             </button>
-
           </div>
 
           {activeTab === "login" && (
             <div className="form">
-              <input type="email" placeholder="Email" className="form-control mb-3" />
-              <input type="password" placeholder="Password" className="form-control mb-3" />
+              <input
+                type="email"
+                placeholder="Email"
+                className="form-control mb-3"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="form-control mb-3"
+              />
               <button className="btn btn-primary w-100">Login</button>
             </div>
           )}
 
           {activeTab === "register" && (
             <div className="form">
-              <input type="text" placeholder="Name" className="form-control mb-3" />
-              <input type="email" placeholder="Email" className="form-control mb-3" />
-              <input type="password" placeholder="Password" className="form-control mb-3" />
+              <input
+                type="text"
+                placeholder="Name"
+                className="form-control mb-3"
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                className="form-control mb-3"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="form-control mb-3"
+              />
 
               <div class="mb-3">
                 <select class="form-select" id="selectOption" name="option">
-                    <option selected>Select Role</option>
-                    <option>Student</option>
-                    <option>Instructor</option>
-                    <option>Admin</option>
+                  <option selected>Select Role</option>
+                  <option>Student</option>
+                  <option>Instructor</option>
+                  <option>Admin</option>
                 </select>
-                </div>
-
+              </div>
 
               <button className="btn btn-primary w-100">Register</button>
             </div>
           )}
-
         </div>
 
         <div className="auth-right">
@@ -66,9 +78,7 @@ function Auth() {
             alt="education"
           />
         </div>
-
       </div>
-
     </div>
   );
 }
