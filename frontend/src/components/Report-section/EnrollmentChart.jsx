d3e203c1d0d0ce238f2e36f119455958bc0b1ce3
Aarchi <aarchi.k@ahduni.edu.in>
2026-03-31 11:15:48 +0530

    Admin dashboard

--- frontend/src/components/Report-section/EnrollmentChart.jsx
+++ frontend/src/components/Report-section/EnrollmentChart.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  CartesianGrid,
-  ResponsiveContainer,
-} from "recharts";
+// import React from 'react'
+// import {
+//   BarChart,
+//   Bar,
+//   XAxis,
+//   YAxis,
+//   Tooltip,
+//   CartesianGrid,
+//   ResponsiveContainer,
+// } from "recharts";
 
 
-export default function EnrollmentChart({data}) {
-  return (
-  <div style={{ flex: 1, background: "#fff", padding: "20px", borderRadius: "12px" }}>
-      <h3>Enrollments</h3>
+// export default function EnrollmentChart({data}) {
+//   return (
+//   <div style={{ flex: 1, background: "#fff", padding: "20px", borderRadius: "12px" }}>
+//       <h3>Enrollments</h3>
 
-      <ResponsiveContainer width="100%" height={300}>
-        <BarChart data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="course" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="enrollments" />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
-  )
-}
+//       <ResponsiveContainer width="100%" height={300}>
+//         <BarChart data={data}>
+//           <CartesianGrid strokeDasharray="3 3" />
+//           <XAxis dataKey="course" />
+//           <YAxis />
+//           <Tooltip />
+//           <Bar dataKey="enrollments" />
+//         </BarChart>
+//       </ResponsiveContainer>
+//     </div>
+//   )
+// }
--- frontend/src/components/Report-section/ReportCard.jsx
+++ frontend/src/components/Report-section/ReportCard.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+// import React from 'react'
 
-export default function ReportCard({title , value  , icon}) {
-  return (
-    <div className="report-card">
-  <div className="card-icon">{icon}</div>
+// export default function ReportCard({title , value  , icon}) {
+//   return (
+//     <div className="report-card">
+//   <div className="card-icon">{icon}</div>
 
-  <div className="card-content">
-    <h2>{value}</h2>
-    <p>{title}</p>
-  </div>
-</div>
-  )
-}
+//   <div className="card-content">
+//     <h2>{value}</h2>
+//     <p>{title}</p>
+//   </div>
+// </div>
+//   )
+// }
